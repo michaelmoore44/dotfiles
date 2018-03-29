70c930f85818e26b0915744cd416e0da14675c6a
Michael Moore <michaelmoore44@gmail.com>
2018-03-29 09:08:00 -0500

    added gdbinit and ll alias

--- df_install.sh
+++ df_install.sh
@@ -7,6 +7,7 @@ dir=~/.dotfiles
 ln -s $dir/tmux/.tmux.conf ~/.tmux.conf
 ln -s $dir/bash/bashrc ~/.bashrc
 ln -s $dir/git/gitconfig ~/.gitconfig
+ln -s $dir/gdb/gdbinit ~/.gdbinit
 
 echo "and done!"
 
